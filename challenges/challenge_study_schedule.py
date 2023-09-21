e73071bedea93b9f81f297ea7829e2f0f2280e89
lucopdev <lucopdev@gmail.com>
2023-09-21 00:15:38 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    counter = 0
+
+    for min_hour, max_hour in permanence_period:
+        if (
+            not permanence_period
+            or not target_time
+            or not min_hour
+            or not max_hour
+            or not isinstance(min_hour, int)
+            or not isinstance(max_hour, int)
+        ):
+            return None
+        if min_hour <= target_time <= max_hour:
+            counter += 1
+    return counter
+
     raise NotImplementedError
---
+++ challenges/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+def study_schedule(permanence_period, target_time):
+    soma = permanence_period + target_time
+    print(soma)
+    raise NotImplementedError
+
+study_schedule(10, 2)
